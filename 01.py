5834878d148ce18f0e6d44f2352a5d06d4570a13
Vinicius3103 <61171452+Vinicius3103@users.noreply.github.com>
2020-02-19 22:27:26 -0300

    Update 01.py
    Foi feita a alteração no código.

--- 01.py
+++ 01.py
@@ -1,6 +1,10 @@
-print ("Hello")
+print ("Hello!! My name is Vinicius")
 
+nome = "Vinicius Bucioli"
 
+for i in range(10):
 
-for i is range(10)
-  print (i)
+    if i%2 == 0:
+        print (nome)
+    else:
+        print (nome[:4])
